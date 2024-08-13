8e1768748b694b40eb71988828a0c72f88ba5bb5
Siddhant <126678613+siddhant-tambatkar@users.noreply.github.com>
2024-08-13 19:33:11 +0530

    Update app.py
    ahhhhhhhhhhhhhh

--- Crop_Prediction/app.py
+++ Crop_Prediction/app.py
@@ -240,83 +240,71 @@ def TopFiveLosers():
 
 
 def SixMonthsForecast():
-    month1=[]
-    month2=[]
-    month3=[]
-    month4=[]
-    month5=[]
-    month6=[]
-    for i in commodity_list:
-        crop=SixMonthsForecastHelper(i.getCropName())
-        k=0
-        for j in crop:
-            time = j[0]
-            price = j[1]
-            change = j[2]
-            if k==0:
-                month1.append((price,change,i.getCropName().split("/")[1],time))
-            elif k==1:
-                month2.append((price,change,i.getCropName().split("/")[1],time))
-            elif k==2:
-                month3.append((price,change,i.getCropName().split("/")[1],time))
-            elif k==3:
-                month4.append((price,change,i.getCropName().split("/")[1],time))
-            elif k==4:
-                month5.append((price,change,i.getCropName().split("/")[1],time))
-            elif k==5:
-                month6.append((price,change,i.getCropName().split("/")[1],time))
-            k+=1
-    month1.sort()
-    month2.sort()
-    month3.sort()
-    month4.sort()
-    month5.sort()
-    month6.sort()
-    crop_month_wise=[]
-    crop_month_wise.append([month1[0][3],month1[len(month1)-1][2],month1[len(month1)-1][0],month1[len(month1)-1][1],month1[0][2],month1[0][0],month1[0][1]])
-    crop_month_wise.append([month2[0][3],month2[len(month2)-1][2],month2[len(month2)-1][0],month2[len(month2)-1][1],month2[0][2],month2[0][0],month2[0][1]])
-    crop_month_wise.append([month3[0][3],month3[len(month3)-1][2],month3[len(month3)-1][0],month3[len(month3)-1][1],month3[0][2],month3[0][0],month3[0][1]])
-    crop_month_wise.append([month4[0][3],month4[len(month4)-1][2],month4[len(month4)-1][0],month4[len(month4)-1][1],month4[0][2],month4[0][0],month4[0][1]])
-    crop_month_wise.append([month5[0][3],month5[len(month5)-1][2],month5[len(month5)-1][0],month5[len(month5)-1][1],month5[0][2],month5[0][0],month5[0][1]])
-    crop_month_wise.append([month6[0][3],month6[len(month6)-1][2],month6[len(month6)-1][0],month6[len(month6)-1][1],month6[0][2],month6[0][0],month6[0][1]])
-
-   # print(crop_month_wise)
+    month_lists = [[] for _ in range(6)]
+    
+    for commodity in commodity_list:
+        crop = SixMonthsForecastHelper(commodity.getCropName())
+        for k in range(min(len(crop), 6)):
+            time, price, change = crop[k]
+            month_lists[k].append((price, change, commodity.getCropName().split("/")[1], time))
+    
+    crop_month_wise = []
+    
+    for month in month_lists:
+        if month:  # Ensure the month list is not empty
+            month.sort()
+            crop_month_wise.append([
+                month[0][3], 
+                month[-1][2], 
+                month[-1][0], 
+                month[-1][1], 
+                month[0][2], 
+                month[0][0], 
+                month[0][1]
+            ])
+        else:
+            crop_month_wise.append([None, None, None, None, None, None, None])
+    
     return crop_month_wise
 
 def SixMonthsForecastHelper(name):
     current_month = datetime.now().month
     current_year = datetime.now().year
     current_rainfall = annual_rainfall[current_month - 1]
-    name = name.split("/")[1]
-    name = name.lower()
-    commodity = commodity_list[0]
+    
+    name = name.split("/")[1].lower()
+    commodity = None
+    
     for i in commodity_list:
-        if name == str(i):
+        if name == str(i).lower():
             commodity = i
             break
+    
+    if not commodity:
+        return []  # Return empty list if commodity is not found
+    
     month_with_year = []
     for i in range(1, 7):
         if current_month + i <= 12:
             month_with_year.append((current_month + i, current_year, annual_rainfall[current_month + i - 1]))
         else:
             month_with_year.append((current_month + i - 12, current_year + 1, annual_rainfall[current_month + i - 13]))
+    
     wpis = []
     current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
     change = []
-
+    
     for m, y, r in month_with_year:
         current_predict = commodity.getPredictedValue([float(m), y, r])
         wpis.append(current_predict)
         change.append(((current_predict - current_wpi) * 100) / current_wpi)
-
+    
     crop_price = []
-    for i in range(0, len(wpis)):
+    for i in range(len(wpis)):
         m, y, r = month_with_year[i]
-        x = datetime(y, m, 1)
-        x = x.strftime("%b %y")
-        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2) , round(change[i], 2)])
-
-   # print("Crop_Price: ", crop_price)
+        x = datetime(y, m, 1).strftime("%b %y")
+        crop_price.append([x, round((wpis[i] * base[name.capitalize()]) / 100, 2), round(change[i], 2)])
+    
     return crop_price
 
 def CurrentMonth(name):
@@ -324,67 +312,68 @@ def CurrentMonth(name):
     current_year = datetime.now().year
     current_rainfall = annual_rainfall[current_month - 1]
     name = name.lower()
-    commodity = commodity_list[0]
+
+    commodity = None
     for i in commodity_list:
-        if name == str(i):
+        if name == str(i).lower():
             commodity = i
             break
+
+    if not commodity:
+        return None  # Return None if commodity is not found
+
     current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
-    current_price = (base[name.capitalize()]*current_wpi)/100
+    current_price = (base[name.capitalize()] * current_wpi) / 100
     return current_price
+
 
 def TwelveMonthsForecast(name):
     current_month = datetime.now().month
     current_year = datetime.now().year
     current_rainfall = annual_rainfall[current_month - 1]
     name = name.lower()
-    commodity = commodity_list[0]
+
+    commodity = None
     for i in commodity_list:
-        if name == str(i):
+        if name == str(i).lower():
             commodity = i
             break
+
+    if not commodity:
+        return None, None, []  # Return None and empty list if commodity is not found
+
     month_with_year = []
     for i in range(1, 13):
         if current_month + i <= 12:
             month_with_year.append((current_month + i, current_year, annual_rainfall[current_month + i - 1]))
         else:
             month_with_year.append((current_month + i - 12, current_year + 1, annual_rainfall[current_month + i - 13]))
-    max_index = 0
-    min_index = 0
-    max_value = 0
-    min_value = 9999
+
     wpis = []
     current_wpi = commodity.getPredictedValue([float(current_month), current_year, current_rainfall])
     change = []
 
     for m, y, r in month_with_year:
         current_predict = commodity.getPredictedValue([float(m), y, r])
-        if current_predict > max_value:
-            max_value = current_predict
-            max_index = month_with_year.index((m, y, r))
-        if current_predict < min_value:
-            min_value = current_predict
-            min_index = month_with_year.index((m, y, r))
         wpis.append(current_predict)
         change.append(((current_predict - current_wpi) * 100) / current_wpi)
 
+    max_index = wpis.index(max(wpis))
+    min_index = wpis.index(min(wpis))
+
     max_month, max_year, r1 = month_with_year[max_index]
     min_month, min_year, r2 = month_with_year[min_index]
-    min_value = min_value * base[name.capitalize()] / 100
-    max_value = max_value * base[name.capitalize()] / 100
+    max_value = wpis[max_index] * base[name.capitalize()] / 100
+    min_value = wpis[min_index] * base[name.capitalize()] / 100
+
     crop_price = []
-    for i in range(0, len(wpis)):
+    for i in range(len(wpis)):
         m, y, r = month_with_year[i]
-        x = datetime(y, m, 1)
-        x = x.strftime("%b %y")
-        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2) , round(change[i], 2)])
-   # print("forecasr", wpis)
-    x = datetime(max_year,max_month,1)
-    x = x.strftime("%b %y")
-    max_crop = [x, round(max_value,2)]
-    x = datetime(min_year, min_month, 1)
-    x = x.strftime("%b %y")
-    min_crop = [x, round(min_value,2)]
+        x = datetime(y, m, 1).strftime("%b %y")
+        crop_price.append([x, round((wpis[i] * base[name.capitalize()]) / 100, 2), round(change[i], 2)])
+
+    max_crop = [datetime(max_year, max_month, 1).strftime("%b %y"), round(max_value, 2)]
+    min_crop = [datetime(min_year, min_month, 1).strftime("%b %y"), round(min_value, 2)]
 
     return max_crop, min_crop, crop_price
 
@@ -394,13 +383,16 @@ def TwelveMonthPrevious(name):
     current_month = datetime.now().month
     current_year = datetime.now().year
     current_rainfall = annual_rainfall[current_month - 1]
-    commodity = commodity_list[0]
-    wpis = []
-    crop_price = []
+
+    commodity = None
     for i in commodity_list:
-        if name == str(i):
+        if name == str(i).lower():
             commodity = i
             break
+
+    if not commodity:
+        return []  # Return empty list if commodity is not found
+
     month_with_year = []
     for i in range(1, 13):
         if current_month - i >= 1:
@@ -408,20 +400,20 @@ def TwelveMonthPrevious(name):
         else:
             month_with_year.append((current_month - i + 12, current_year - 1, annual_rainfall[current_month - i + 11]))
 
+    wpis = []
     for m, y, r in month_with_year:
-        current_predict = commodity.getPredictedValue([float(m), 2013, r])
+        current_predict = commodity.getPredictedValue([float(m), y, r])
         wpis.append(current_predict)
 
-    for i in range(0, len(wpis)):
+    crop_price = []
+    for i in range(len(wpis)):
         m, y, r = month_with_year[i]
-        x = datetime(y,m,1)
-        x = x.strftime("%b %y")
-        crop_price.append([x, round((wpis[i]* base[name.capitalize()]) / 100, 2)])
-   # print("previous ", wpis)
-    new_crop_price =[]
-    for i in range(len(crop_price)-1,-1,-1):
-        new_crop_price.append(crop_price[i])
-    return new_crop_price
+        x = datetime(y, m, 1).strftime("%b %y")
+        crop_price.append([x, round((wpis[i] * base[name.capitalize()]) / 100, 2)])
+
+    crop_price.reverse()  # Reverse the list to show previous months in descending order
+    return crop_price
+
 
 
 if __name__ == "__main__":
